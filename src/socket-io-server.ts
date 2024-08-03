447ba59afdcdda4a2baa6c88f957121369c4998b
Martin Elsaesser <marels@aon.at>
2024-08-03 21:59:51 +0200

    working socket io server

---
+++ src/socket-io-server.ts
@@ -0,0 +1,12 @@
+import { Server as SocketIOServer } from "socket.io";
+
+const socketIOServer = new SocketIOServer();
+
+socketIOServer.on("connection", (socket) => {
+  socket.emit("hello", "world");
+  socket.on("hello", (data) => {
+    socket.broadcast.emit("hello", data);
+  });
+});
+
+export default socketIOServer;
