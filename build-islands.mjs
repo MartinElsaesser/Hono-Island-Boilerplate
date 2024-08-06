d582565a1f3453f642d6796df029e2fab2291dcb
Martin Elsaesser <marels@aon.at>
2024-08-06 09:54:07 +0200

    rework naming

--- build-islands.mjs
+++ build-islands.mjs
@@ -1,19 +1,29 @@
 import * as esbuild from 'esbuild'
 import { readFile } from 'fs/promises';
 
+/**
+ * @file Configures the build step for island components
+ */
+
+/**
+ * load .tsx files  
+ * append packages for client rendering to the code  
+ * and pass the modified files along to the build step
+ */
 let addRenderDepsToIslands = {
-	name: "example",
+	name: "add-render-deps",
 	setup(build) {
-		build.onLoad({ filter: /\.tsx$/ }, async (args) => {
-			let text = await readFile(args.path, "utf-8");
-			text += `\
+		const renderDependencies = `
 				import { render as HonoJsxRender_ClientExpose} from "hono/jsx/dom"
 				import { jsx as HonoJsx_ClientExpose } from "hono/jsx/jsx-runtime";
 				export {HonoJsxRender_ClientExpose, HonoJsx_ClientExpose };
-			`
+			`;
 
+		build.onLoad({ filter: /\.tsx$/ }, async (args) => {
+			let tsxFile = await readFile(args.path, "utf-8");
+			tsxFile += renderDependencies;
 			return {
-				contents: text,
+				contents: tsxFile,
 				loader: "tsx"
 			}
 		});
