eb36a2f2c208253326f22e112b8511459ab8f00e
Martin Elsaesser <marels@aon.at>
2025-04-21 19:49:45 +0200

    adjust server to react

--- src/server.tsx
+++ src/server.tsx
@@ -1,68 +1,68 @@
-import { serve } from '@hono/node-server'
+import { serve } from '@hono/node-server';
 import { serveStatic } from '@hono/node-server/serve-static';
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 import socketIOServer from './socket-io-server.js';
-import { jsxRenderer } from 'hono/jsx-renderer';
+import { reactRenderer } from '@hono/react-renderer';
 import Island from './Island.js';
 import TodoApp from './islands/TodoApp.js';
 import type { Todo } from './islands/TodoApp.js';
 import Counter from './islands/Counter.js';
+import { Suspense } from 'react';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get("/static/*", serveStatic({ root: "./" }))
+app.get('/static/*', serveStatic({ root: './' }));
 
 app.get(
-	'/*',
-	jsxRenderer(({ children }) => {
-		return (
-			<html>
-				<head>
-					<link rel="stylesheet" href="/static/css/app.css" />
-				</head>
-				<body>
-					{children}
-					<script type="module" src="/static/js/hydrate.mjs"></script>
-				</body>
-			</html>
-		)
-	})
-)
+  '/*',
+  reactRenderer(
+    ({ children }) => {
+      return (
+        <html>
+          <head>
+            <link rel="stylesheet" href="/static/css/app.css" />
+          </head>
+          <body>
+            <Suspense fallback={<div>Loading</div>}>{children}</Suspense>
+            <script type="module" src="/static/js/hydrate.mjs"></script>
+          </body>
+        </html>
+      );
+    },
+    { docType: true, stream: true }
+  )
+);
 app.get('/', (c) => {
-	return c.render(
-		<>
-			<h2>Counter 1</h2>
-			<Island pathToComponent='/src/islands/Counter.tsx'>
-				<Counter initialCount={4}></Counter>
-			</Island>
-			<h2>Counter 2</h2>
-			<Island pathToComponent='/src/islands/Counter.tsx'>
-				<Counter initialCount={2}></Counter>
-			</Island>
-		</>
-	)
+  return c.render(
+    <>
+      <h2>Counter 1</h2>
+      <Island pathToComponent="/src/islands/Counter.tsx">
+        <Counter initialCount={4}></Counter>
+      </Island>
+      <h2>Counter 2</h2>
+      <Island pathToComponent="/src/islands/Counter.tsx">
+        <Counter initialCount={2}></Counter>
+      </Island>
+    </>
+  );
 });
 
 app.get('/todos', (c) => {
-	const todos: Todo[] = [
-		{ head: "Milk the cow", done: false },
-		{ head: "Watch Youtube", done: false },
-		{ head: "Build todo app", done: false },
-	]
-	return c.render(
-		<Island pathToComponent='/src/islands/TodoApp.tsx'>
-			<TodoApp _todos={todos}></TodoApp>
-		</Island>
-	)
+  const todos: Todo[] = [
+    { head: 'Milk the cow', done: false },
+    { head: 'Watch Youtube', done: false },
+    { head: 'Build todo app', done: false },
+  ];
+  return c.render(
+    <Island pathToComponent="/src/islands/TodoApp.tsx">
+      <TodoApp _todos={todos}></TodoApp>
+    </Island>
+  );
 });
 
-const port = 3000
-const server = serve(
-	{ fetch: app.fetch, port },
-	(info) => {
-		console.log(`Server started on http://localhost:${port}`);
-	}
-);
+const port = 3000;
+const server = serve({ fetch: app.fetch, port }, (info) => {
+  console.log(`Server started on http://localhost:${port}`);
+});
 
-socketIOServer.attach(server)
-
+socketIOServer.attach(server);
