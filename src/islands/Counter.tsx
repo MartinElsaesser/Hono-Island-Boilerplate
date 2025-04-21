c5ac9922fbb9f2ab24b351d3be45df6a91e90046
Martin Elsaesser <marels@aon.at>
2025-04-21 19:40:49 +0200

    move islands to react

--- src/islands/Counter.tsx
+++ src/islands/Counter.tsx
@@ -1,16 +1,15 @@
-import { useState } from "hono/jsx";
+import { useState } from 'react';
 
 export default function Counter({ initialCount }: { initialCount: number }) {
-	const [count, setCount] = useState(initialCount);
-	
-	return (
-		<div>
-			<div>
-				<button onClick={e => setCount(c => c-1)}>-</button>
-				<span>{count}</span>
-				<button onClick={e => setCount(c => c+1)}>+</button>
-			</div>
-		</div>
-	)
-}
+  const [count, setCount] = useState(initialCount);
 
+  return (
+    <div>
+      <div>
+        <button onClick={(e) => setCount((c) => c - 1)}>-</button>
+        <span>{count}</span>
+        <button onClick={(e) => setCount((c) => c + 1)}>+</button>
+      </div>
+    </div>
+  );
+}
