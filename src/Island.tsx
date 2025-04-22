7cf917335e57e2b88c1af1c0134b63238a8f3ee6
Martin Elsaesser <marels@aon.at>
2025-04-22 22:21:26 +0200

    wip: adjust Island component to use the component's hydration id

--- src/Island.tsx
+++ src/Island.tsx
@@ -1,71 +1,10 @@
 import React from 'react';
 import serialize from 'serialize-javascript';
 
-/**
- *
- * throw an error if the island component violates either criteria:
- * * IslandComponent is a functional component
- * * default export from pathToComponent equals island component
- * @param pathToComponent - path to island component
- * @param IslandComponent - island component
- */
-async function verifyComponentImport(
-  pathToComponent: string,
-  IslandComponent: React.ReactElement
-) {
-  // default import the contents from pathToComponent
-  // default import is expected to equal IslandComponent
-  const relativePathToComponent = pathToComponent.slice('/src/'.length);
-  const { default: ImportedComponent } = await import(
-    `./${relativePathToComponent}`
-  );
-
-  // throw error if IslandComponent is not a functional component
-  if (typeof IslandComponent.type !== 'function') {
-    throw new Error(
-      `Island component "${IslandComponent.type}" is not a functional component`
-    );
-  }
-
-  // throw error if IslandComponent does not match Imported Component
-  if (IslandComponent.type !== ImportedComponent) {
-    throw new Error(
-      `Island component "${IslandComponent.type.name}" does not match the one from pathToComponent "${ImportedComponent.name}"`
-    );
-  }
-}
-
-/**
- *
- * throw an error if the path violates either criteria:
- * * path matches `/src/islands/.../[name].tsx`
- * * and does not contain /../
- * @param path - path to an island component
- */
-function throwOnFaultyComponentPath(path: string) {
-  const absolutePathToIslandsDir = /^\/src\/islands\/.*\.tsx$/;
-  const goBacksInPath = /\/..\//;
-
-  // throw error if pathToComponent differs from /src/islands/.../[name].tsx
-  if (!absolutePathToIslandsDir.test(path)) {
-    // ** means zero or more directories
-    throw new Error(
-      `{pathToComponent: "${path}"} needs to macht "/src/islands/**/[name].tsx"`
-    );
-  }
-
-  // throw error if /../ is found in componentPath
-  if (goBacksInPath.test(path)) {
-    throw new Error(`{pathToComponent:"${path}"} contains illegal "/../"`);
-  }
-}
-
 export default async function Island({
   children,
-  pathToComponent,
 }: {
   children: React.ReactElement;
-  pathToComponent: string;
 }) {
   // goal: only allow imports matching /src/islands/**/[name].tsx
   // if goal is not met: throw an error
@@ -76,15 +15,18 @@ export default async function Island({
     if (!React.isValidElement(children)) {
       throw Error('only components are valid children');
     }
-    throwOnFaultyComponentPath(pathToComponent);
-    await verifyComponentImport(pathToComponent, children);
-    const regex_filePathNoExt = /^\/src\/islands\/(.*)\.tsx$/;
-    const filePathNoExt = regex_filePathNoExt.exec(pathToComponent)![1];
+
+    // throw error if IslandComponent is not a functional component
+    if (typeof children.type !== 'function') {
+      throw new Error(
+        `Island component "${children.type}" is not a functional component`
+      );
+    }
 
     return (
       // TODO: serialize `children.props` such that Maps, Sets and Dates are supported
       <div
-        data-hydration-src={`/static/js/islands/${filePathNoExt}.js`}
+        data-hydration-src={children.type.hydrationId}
         data-hydration-props={JSON.stringify(children.props)}
       >
         {children}
