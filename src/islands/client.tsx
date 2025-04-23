8689f0f7ec08587ef68b093cae8ea034ce50ff79
Martin Elsaesser <marels@aon.at>
2025-04-23 21:49:08 +0200

    refactor: integrate zod for improved validation for data-hydration-island-idx and data-hydration-props

--- src/islands/client.tsx
+++ src/islands/client.tsx
@@ -1,25 +1,35 @@
 import { registeredIslands } from './shared.js';
+import z from 'zod';
 
 // provide necessary imports for hydration
 import { createRoot } from 'react-dom/client';
 import { jsx } from 'react/jsx-runtime';
 
-// TODO: remove ! and handle not found cases
 const islandWrappers = Array.from(
   document.querySelectorAll('[data-hydration-island-idx]')
 );
 
+const strToIntSchema = z
+  .string()
+  .regex(/^\d+$/)
+  .transform((val) => parseInt(val, 10));
+const propsSchema = z.string().transform((val) => JSON.parse(val));
+
 if (islandWrappers.length > 0) {
   for (const wrapper of islandWrappers) {
-    // TODO: add explicit type conversion
-    const src = +wrapper.getAttribute('data-hydration-island-idx')!;
-    console.log(`hydrating ${src}`);
+    const rawIslandIdx = wrapper.getAttribute('data-hydration-island-idx');
+    const { data: islandIdx, error: error1 } =
+      strToIntSchema.safeParse(rawIslandIdx);
+    if (error1) throw new Error(`Invalid island index: ${rawIslandIdx}`);
 
-    const props = JSON.parse(wrapper.getAttribute('data-hydration-props')!);
-    const root = createRoot(wrapper);
+    const rawProps = wrapper.getAttribute('data-hydration-props');
+    const { data: props, error: error2 } = propsSchema.safeParse(rawProps);
+    if (error2) throw new Error(`Empty props: ${rawProps}`);
 
     // find matching island in the islands object
-    const island = registeredIslands[src];
+    const island = registeredIslands[islandIdx];
+
+    const root = createRoot(wrapper);
     root.render(jsx(island, props));
   }
 }
