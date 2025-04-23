5fb4d9fa53eb5661f17d996dcf5b3f6d154cc237
Martin Elsaesser <marels@aon.at>
2025-04-23 20:44:31 +0200

    refactor: simplify build process by removing unused CSR plugin and updating entry points

--- build-islands.mjs
+++ build-islands.mjs
@@ -2,56 +2,13 @@
 import * as esbuild from 'esbuild';
 import { readFile, rm, rmdir } from 'fs/promises';
 
-/*
- * @file
- * Contains the build step for island components:
- * 1. take files matching /src/islands/[Name].tsx
- * 2. add dependencies for client side rendering
- * 3. bundle the files and transform them to javascript
- * 4. save them to /static/js/islands/[Name].js.
- *
- * rendering of the island components is handled in /static/js/hydrate.mjs
- * @param "--watch" - starts the build in watch mode
- */
-
-/**
- * plugin adds packages for client side rendering (CSR)
- */
-
-/** @type {import('esbuild').Plugin} */
-let plugin_addCSRPackages = {
-  name: 'add-csr-deps',
-  setup(build) {
-    const renderDependencies = `
-		import { createRoot } from 'react-dom/client';
-		import { jsx } from 'react/jsx-runtime';
-		export { jsx as __jsx__, createRoot as __createRoot__ };
-	`;
-
-    build.onLoad({ filter: /\.tsx$/ }, async (args) => {
-      // load tsx file
-      let tsxFile = await readFile(args.path, 'utf-8');
-
-      // append packages for client side rendering
-      tsxFile += renderDependencies;
-
-      // pass component on to the build step
-      return {
-        contents: tsxFile,
-        loader: 'tsx',
-      };
-    });
-  },
-};
-
 /** @type {import('esbuild').BuildOptions} */
 const buildOptions = {
-  entryPoints: ['src/islands/**/*.tsx'],
+  entryPoints: ['src/islands/islands.tsx'],
   bundle: true,
   minify: false,
   format: 'esm',
   outdir: '/static/js/islands',
-  plugins: [plugin_addCSRPackages],
   chunkNames: '[name]-[hash]',
   splitting: true,
 };
--- src/islands/islands.tsx
+++ src/islands/islands.tsx
@@ -2,4 +2,10 @@ import Counter from './Counter.js';
 import TodoApp from './TodoApp.js';
 import React from 'react';
 
+// register islands
 export default [Counter, TodoApp];
+
+// provide necessary imports for hydration
+import { createRoot } from 'react-dom/client';
+import { jsx } from 'react/jsx-runtime';
+export { jsx as __jsx__, createRoot as __createRoot__ };
