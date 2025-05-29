943cde951288451a52c2f3cf638d35c250a3caff
Martin Elsaesser <marels@aon.at>
2025-05-29 14:23:12 +0200

    wip: client hydration

--- src/islands/client.tsx
+++ src/islands/client.tsx
@@ -1,6 +1,4 @@
 // --- imports
-// get all react components that are registered to run on the client
-import { registeredIslands } from "./shared.js";
 
 // provide necessary imports to render islands on the client
 import { parse } from "superjson";
@@ -10,17 +8,17 @@ import { jsx } from "react/jsx-runtime";
 // zod schemas for validating island information
 import { z } from "zod";
 
-export const islandIndexSchema = z
-	.string()
-	.regex(/^\d+$/)
-	.transform(val => parseInt(val, 10))
-	.refine(index => registeredIslands[index] !== undefined);
-
 export const islandPropsSchema = z.string().transform(val => parse(val));
+
+export const islandSchema = z.object({
+	islandProps: islandPropsSchema,
+	islandPath: z.string().min(1),
+	islandImport: z.string().min(1),
+});
 
 // --- code for hydration, i.e. rendering islands on the client
 // get all html elements that wrap islands
-const islandWrappers = Array.from(document.querySelectorAll("[data-island-index]"));
+const islandWrappers = Array.from(document.querySelectorAll("[data-island-path]"));
 
 if (islandWrappers.length > 0) {
 	for (const wrapper of islandWrappers) {
@@ -28,18 +26,14 @@ if (islandWrappers.length > 0) {
 		// island index will tell us which island to render
 		// props will be used to achieve the same state as on the server
 
-		const rawIslandIndex = wrapper.getAttribute("data-island-index");
-		const { data: islandIndex, error: error1 } = islandIndexSchema.safeParse(rawIslandIndex);
-		if (error1) throw new Error(`Invalid island index: ${rawIslandIndex}`);
-
-		const rawProps = wrapper.getAttribute("data-island-props");
-		const { data: props, error: error2 } = islandPropsSchema.safeParse(rawProps);
-		if (error2) throw new Error(`Empty props: ${rawProps}`);
+		const { islandImport, islandPath, islandProps } = islandSchema.parse(wrapper.dataset);
 
 		// find matching island in the islands object
-		const island = registeredIslands[islandIndex];
+		const island = (await import(islandPath)).default;
+
+		console.log(island);
 
 		const root = createRoot(wrapper);
-		root.render(jsx(island, props));
+		root.render(jsx(island, islandProps));
 	}
 }
