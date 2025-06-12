f0c844515e5d5bf60bc999a8ef5eee56d17430bf
Martin Elsaesser <marels@aon.at>
2025-06-12 12:31:09 +0200

    disallow nesting Island components and improve error handling

--- src/islands/server.tsx
+++ src/islands/server.tsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { createContext, use, useContext } from "react";
 import superjson from "superjson";
-import { isIslandComponent, ISLAND_BUILD_PATH, ISLAND_INDEX } from "./client.js";
+import { isIslandComponent, ISLAND_BUILD_PATH, ISLAND_INDEX, registerIslands } from "./client.js";
+
+const RegistrationContext = createContext(false);
 
 export default function Island({ children }: { children: React.ReactElement }) {
 	try {
 		// TODO: format zod error messages in a readable way
 
-		// TODO: check that the Island component is not within another Island component (use a context to track this)
+		const withinAnotherIsland = useContext(RegistrationContext);
+		if (withinAnotherIsland) {
+			throw Error("Island components cannot be nested within each other.");
+		}
 
 		if (!React.isValidElement(children)) {
 			throw Error("Not a valid React element.");
 		}
 
 		if (!isIslandComponent(children.type)) {
+			const identifier =
+				typeof children.type === "function" ? children.type.name : children.type;
 			throw new Error(
-				`"${children.type}" is not a user defined component, or it was not registered correctly with registerIslands().`
+				`"${identifier}" is not a user defined component, or it was not registered correctly with registerIslands().`
 			);
 		}
 
 		return (
-			<>
+			<RegistrationContext value={true}>
 				<div
 					data-island-props={superjson.stringify(children.props)}
 					data-island-build-path={children.type[ISLAND_BUILD_PATH]}
@@ -28,10 +35,11 @@ export default function Island({ children }: { children: React.ReactElement }) {
 					{children}
 				</div>
 				<script src={children.type[ISLAND_BUILD_PATH]} type="module"></script>
-			</>
+			</RegistrationContext>
 		);
 	} catch (error: unknown) {
 		// display errors
+		console.error(error);
 		let message = "";
 
 		if (typeof error === "string") {
