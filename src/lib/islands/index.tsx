e0eb5df3864e7cf8c672e025f1beb0f9a0239982
Martin Elsaesser <marels@aon.at>
2025-06-12 18:39:03 +0200

    add todos

--- src/lib/islands/index.tsx
+++ src/lib/islands/index.tsx
@@ -1,3 +1,17 @@
 export { Island } from "./server.js";
 
 export { registerIslands } from "./shared.js";
+
+// TODO: add config
+// - server file paths to components on disk
+// - server file paths to build folder
+// - base url to served build folder
+
+// TODO: follow up to config
+// when config is implemented, registerIslands() should check if the file is included in the "server file paths to components on disk"
+// when config is implemented, resolveComponentBuildPath() should use the config to resolve the build path
+
+// TODO: should the code also work for common js? - would mean:
+// - no code splitting
+// - import.meta is not available
+// probably impossible without import.meta
