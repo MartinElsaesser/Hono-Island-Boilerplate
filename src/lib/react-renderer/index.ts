336502ba4a663284c5960c21381ba13c3246b302
Martin Elsaesser <marels@aon.at>
2025-08-02 15:34:49 +0200

    remove files (were previously used for debugging)

--- src/lib/react-renderer/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./react-renderer.js";
-
-export interface Props {}
-
-declare module "hono" {
-	interface ContextRenderer {
-		(children: React.ReactElement, props?: Props): Response | Promise<Response>;
-	}
-}
